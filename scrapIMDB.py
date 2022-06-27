e928cb9b2d24e799ce044a681e961d26bc78ae3a
aliza <aliza1400@gmail.com>
2022-06-27 14:48:02 +0300

    try added

--- scrapIMDB.py
+++ scrapIMDB.py
@@ -6,29 +6,34 @@ from urllib import response
 from bs4 import BeautifulSoup
 import requests
 
+
+
+try:
+
 #requesr
 
-web_site = "https://www.imdb.com/chart/top/"
-response = requests.get(web_site)
-html = response.text
+    web_site = "https://www.imdb.com/chart/top/"
+    response = requests.get(web_site)
+    html = response.text
 
-#soup
+    #soup
 
-soup = BeautifulSoup(html, 'html.parser')
-#print(soup.prettify())
+    soup = BeautifulSoup(html, 'html.parser')
+    #print(soup.prettify())
 
-#find&findall
-#fing all movies
-movies = soup.find('tbody', class_="lister-list").find_all('tr')
-#print(movies)
+    #find&findall
+    #fing all movies
+    movies = soup.find('tbody', class_="lister-list").find_all('tr')
+    #print(movies)
 
-for movie in movies:
+    for movie in movies:
 
-    name = movie.find('td',class_="titleColumn").a.text
-    rank = movie.find('td',class_="titleColumn").get_text(strip=True).split('.')[0]
-    year = movie.find('td',class_="titleColumn").find('span').text.strip('()')
-    rate = movie.find('td', class_="ratingColumn imdbRating").text
-    print('rank: ' ,rank, "name: " ,name, "year: " , year ,"rate: ",rate)
+        name = movie.find('td',class_="titleColumn").a.text
+        rank = movie.find('td',class_="titleColumn").get_text(strip=True).split('.')[0]
+        year = movie.find('td',class_="titleColumn").find('span').text.strip('()')
+        rate = movie.find('td', class_="ratingColumn imdbRating").text
+        print('rank: ' ,rank, "name: " ,name, "year: " , year ,"rate: ",rate)
 
-
+except Exception as e:
+    print(e)
 
